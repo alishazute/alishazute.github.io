38293c86756a93a76a7ef251d96efa17e6ee88bc
alishazute <42978508+alishazute@users.noreply.github.com>
2020-04-18 04:19:22 -0500

    column stacking issue fix instagram

--- js/get_latest_post.js
+++ js/get_latest_post.js
@@ -1,14 +1,11 @@
+$.ajax("blog_posts/" + 3 + ".txt").done(displayText).fail(showError);
 
 
-	$.ajax("blog_posts/"+3+".txt").done(displayText).fail(showError);
-
-
-
-
-function displayText(data){
-    $("#latest_post").append(data);
+function displayText(data) {
+  $("#latest_post").append(data);
 }
-function showError(data){
-   var mess = "An error occured!";
-   $("#latest_post").text(mess);
+
+function showError(data) {
+  var mess = "An error occured!";
+  $("#latest_post").text(mess);
 }
--- js/get_post.js
+++ js/get_post.js
@@ -1,43 +1,23 @@
+for (i = 1; i <= 3; i++) {
+  get_num_post(i);
 
-for (i = 1; i <= 3; i++) 
-	{
-		get_num_post(i);
-		
-
-	}
-
-
-
-
-
-
-function get_num_post(x){
-
-	$.ajax("blog_posts/"+x+".txt").done(displayText).fail(showError);
 
 }
 
 
+function get_num_post(x) {
 
-function displayText(data){
-    $("#blog_post").append(data);
-    $("#blog_post").append("<br/><br/><br/>");
-}
-function showError(data){
-   var mess = "An error occured!";
-   $("#blog_post").text(mess);
+  $.ajax("blog_posts/" + x + ".txt").done(displayText).fail(showError);
+
 }
 
 
+function displayText(data) {
+  $("#blog_post").append(data);
+  $("#blog_post").append("<br/><br/><br/>");
+}
 
-
-
-
-
-
-
-
-
-
-
-
+function showError(data) {
+  var mess = "An error occured!";
+  $("#blog_post").text(mess);
+}
